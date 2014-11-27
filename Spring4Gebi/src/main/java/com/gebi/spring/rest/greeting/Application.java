7896d008dc89ec7e145b02710c788d79d7d71979
Gebi83 <dampf.hans13@googlemail.com>
2014-11-27 08:11:47 +0100

    initial commit Spring4Integration

--- Spring4Gebi/src/main/java/com/gebi/spring/rest/greeting/Application.java
+++ Spring4Gebi/src/main/java/com/gebi/spring/rest/greeting/Application.java
@@ -3,13 +3,22 @@ package com.gebi.spring.rest.greeting;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.client.RestTemplate;
 
 @ComponentScan
 @EnableAutoConfiguration
 public class Application {
 	
 	public static void main(String[] args){
+
+        RestTemplate restTemplate = new RestTemplate();
+        Page page = restTemplate.getForObject("http://graph.facebook.com/pivotalsoftware", Page.class);
+        System.out.println("Name:    " + page.getName());
+        System.out.println("About:   " + page.getAbout());
+        System.out.println("Phone:   " + page.getPhone());
+        System.out.println("Website: " + page.getWebsite());
 		SpringApplication.run(Application.class, args);
 	}
+	
 
 }
